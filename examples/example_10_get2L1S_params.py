b734b025b28015d7fd236a5ea93b54755445d8a1
jenniferyee <jennifer.c.yee@gmail.com>
2026-06-08 14:53:35 -0400

    Updated ex10 to match new architecture. Workds.

--- examples/example_10_get2L1S_params.py
+++ examples/example_10_get2L1S_params.py
@@ -6,24 +6,25 @@ u0        alpha       t0         tE        rE       thetaE    piE     rhos
 0.00273006 49.2163 83.7988 270.63 0.0109202 2.48124 686.808
 """
 
-from examples.data_for_test_examples import datasets
-import exozippy as mmexo
+from data_for_test_examples import datasets
+import mmexofast as mmexo
 import MulensModel
 import matplotlib.pyplot as plt
 
+
 pspl_params = {'t_0': 2460024.676357266, 't_E': 6.96083126,
-               'u_0': 1.4639013963602456, 'chi2': -99}
+               'u_0': 1.4639013963602456}
 af_grid_params = {'t_0': 2460017.625, 't_eff': 0.75, 'j': 2.0,
                   'chi2': 395.2493798573346, 'dchi2_zero': 5563.0445050189755,
                   'dchi2_flat': 4186.45789320002}
 
-fitter = mmexo.mmexofast.MMEXOFASTFitter(datasets=datasets)
-fitter.initial_pspl_results = pspl_params
-fitter.best_af_grid_point = af_grid_params
-#fitter.set_residuals(fitter.pspl_params)
-#fitter.set_datasets_with_anomaly_masked()
-anomaly_lc_params = fitter.get_anomaly_lc_params()
-binary_params = mmexo.estimate_params.get_wide_params(anomaly_lc_params)
+
+anomaly_estimator = mmexo.estimate_params.AnomalyPropertyEstimator(
+    datasets=datasets, pspl_params=pspl_params, af_results=af_grid_params
+)
+param_estimator = mmexo.estimate_params.WidePlanetParameterEstimator(
+    anomaly_estimator.get_anomaly_lc_parameters())
+binary_params = param_estimator.get_binary_lens_params()
 print(binary_params.ulens)
 print(binary_params.mag_methods)
 
